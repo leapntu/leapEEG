18b997f5aa653a8a0a679acae1d22f064184d9ae
skagit <matthewemagnuson@gmail.com>
2016-03-15 17:15:06 +0800

    timing adjustment and dialog

--- dualgram_words_multimodal.py
+++ dualgram_words_multimodal.py
@@ -3,31 +3,49 @@ import pyglet, os, random, copy
 from pyo import *
 
 ###ENVIRONMENT AND LOADING###
-win = visual.Window()
+win = visual.Window([1280,1024])
 
 loadMessage = visual.TextStim(win, text="Loading Stimuli\n\nPlease Wait")
 loadMessage.draw()
 
 win.flip()
 
+info = []
+
+def get_metadata():
+  global info
+  myDlg = gui.Dlg(title="JWP's experiment")
+  myDlg.addText('Participant Information')
+  myDlg.addField('Name:')
+  myDlg.addField('Age:')
+  myDlg.addText('Experiment Information')
+  myDlg.addField('RA:')
+  myDlg.show()
+  if myDlg.OK:
+      info = myDlg.data
+  else:
+      print 'user cancelled'
+      
+get_metadata()
+
 ###CONSTANTS AND PARAMETERS###
 
-introMessage = "DUAL GRAMMAR TASK - INSTRUCTIONS TO PARTICIPANTS AND ORDER OF PRESENTATION OF PARTS OF EXPERIMENT\nHello! Welcome to our cognitive brain training game! Test your attention abilities and the way you can improve them! If you complete the following tasks successfully, you may improve your cognitive abilities!\nThe game has 6 rounds, lasting approximately 12 minutes overall. Your task is to pay close attention to the sequences of words. It is important to pay attention to the words as you will be tested on what you have heard later on! We believe that though this task is demanding, it will pay off.\nYou will be given a break at the end of each round. Please rest for as long as you need to during the breaks given, or continue to the next round if you are able to power through!\nPlease ensure that you are in a quiet room and have earphones to listen to the words before starting the task.\nPress <b>SPACEBAR</b> when you are ready."
+introMessage = "Welcome to our cognitive brain training game! Test your attention abilities and the way you can improve them! If you complete the following tasks successfully, you may improve your cognitive abilities!\nThe game has 6 rounds, lasting approximately 12 minutes overall. Your task is to pay close attention to the sequences of words. It is important to pay attention to the words as you will be tested on what you have heard later on! We believe that though this task is demanding, it will pay off.\nYou will be given a break at the end of each round. Please rest for as long as you need to during the breaks given, or continue to the next round if you are able to power through!\nPlease ensure that you are in a quiet room and have earphones to listen to the words before starting the task.\nPress SPACEBAR when you are ready."
 
-restMessage = "You can now take a break for as long as you need to before continuing.\nPlease press <b>SPACEBAR</b> when you are ready to continue."
+restMessage = "You can now take a break for as long as you need to before continuing.\nPlease press SPACEBAR when you are ready to continue."
 
 halfwayMessage = "WELL DONE! You are halfway done!\n"+restMessage
 
-preTestMessage = "Well done!\nThe  sequences that you have just heard were generated according to a set of rules that determined the order of words within each sequence.\nYou will now hear a new set of  sequences. Half of these sequences will conform to the same set of rules as before, and the rest will not. Your task is to judge which of the sequences follow the same rules as before and which do not.\nIf the  sequence follows the same roles as before, press <b>Y</b>.\nIf the  sequence does not follow the same rules as before,\npress <b>N</b>.\nIf you are unsure, please respond with your gut feeling.\nPress <b>SPACEBAR</b> when you are ready to begin."
+preTestMessage = "Well done!\nThe  sequences that you have just heard were generated according to a set of rules that determined the order of words within each sequence.\nYou will now hear a new set of  sequences. Half of these sequences will conform to the same set of rules as before, and the rest will not. Your task is to judge which of the sequences follow the same rules as before and which do not.\nIf the  sequence follows the same roles as before, press Y.\nIf the  sequence does not follow the same rules as before,\npress N.\nIf you are unsure, please respond with your gut feeling.\nPress SPACEBAR when you are ready to begin."
 
-testMessage = "Did the  sequence follow the same rules as before?\n<b>Y (YES)</b>                                     <b>N (NO)</b>"
+testMessage = "Did the  sequence follow the same rules as before?\nY (YES)                                     N (NO)"
 
 goodbyeMessage = "You have now come to the end of our experiment.\nFor more information on our study, please refer to our debrief notes.\nThank you for your time and participation!"
 
-intro = visual.TextStim(win, text=introMessage)
+intro = visual.TextStim(win, text=introMessage, height = .07, wrapWidth = 1.5)
 rest = visual.TextStim(win, text=restMessage)
 halfway = visual.TextStim(win, text=halfwayMessage)
-preTest = visual.TextStim(win, text=preTestMessage)
+preTest = visual.TextStim(win, text=preTestMessage, height = .07, wrapWidth = 1.5)
 test = visual.TextStim(win, text=testMessage)
 goodbye = visual.TextStim(win, text=goodbyeMessage)
 
@@ -36,18 +54,32 @@ gramBSymbols = 'P Q W Y Z'.split()
 
 expDir = '/home/leapadmin/Desktop/leapEEG/'
 stimuliDir = expDir + 'stimuli/'
-gramADir = stimuliDir + 'audio/gram_a/'
-gramBDir = stimuliDir + 'audio/gram_b/'
 
-gramAFiles = os.listdir(gramADir)
-gramBFiles = os.listdir(gramBDir)
+gramAVisualDir = stimuliDir + 'visual/gram_a/'
+gramBVisualDir = stimuliDir + 'visual/gram_b/'
+
+gramAAudioDir = stimuliDir + 'audio/gram_a/'
+gramBAudioDir = stimuliDir + 'audio/gram_b/'
+
+
+gramAAudioFiles = os.listdir(gramAAudioDir)
+gramBAudioFiles = os.listdir(gramBAudioDir)
+
+gramAVisualFiles = os.listdir(gramAVisualDir)
+gramBVisualFiles = os.listdir(gramBVisualDir)
 
 blockFile = 'stimuli/metadata/order.txt'
 
-gramAStims = [ sound.Sound(gramADir+filename) for filename in gramAFiles ]
-gramBStims = [ sound.Sound(gramBDir+filename) for filename in gramBFiles ]
+gramAStimsVisual = [ visual.ImageStim(win, gramAVisualDir+filename) for filename in gramAVisualFiles ]
+gramBStimsVisual = [ visual.ImageStim(win, gramBVisualDir+filename) for filename in gramBVisualFiles ]
+
+gramAStimsAudio = [ sound.Sound(gramAAudioDir+filename) for filename in gramAAudioFiles ]
+gramBStimsAudio = [ sound.Sound(gramBAudioDir+filename) for filename in gramBAudioFiles ]
 
 fixation = visual.ImageStim(win, stimuliDir + 'fix.svg.png')
+
+#enable parallel port access with:
+#sudo modprobe -r lp
 
 port = parallel.ParallelPort('/dev/parport0')
 core.wait(2)
@@ -126,45 +158,49 @@ def setSymbols():
     lookupDict = {}
     random.shuffle(gramASymbols)
     random.shuffle(gramBSymbols)
-    for symbol, stimuli in zip(gramASymbols, gramAStims):
-        lookupDict[symbol] = stimuli
-    for symbol, stimuli in zip(gramBSymbols, gramBStims):
-        lookupDict[symbol] = stimuli
+    for symbol, visual, audio in zip(gramASymbols, gramAStimsVisual, gramAStimsAudio):
+        lookupDict[symbol] = [audio, visual]
+    for symbol, visual, audio in zip(gramBSymbols, gramBStimsVisual, gramBStimsAudio):
+        lookupDict[symbol] = [audio, visual]
     return lookupDict
 
 #Experiment control functions
 def playBite(bite):
+    code = bite['code']
+    duration = 0.7
+    isi = .1
+    if code != 21:
+      isi = .3
+      duration = 1
     for symbol in bite['symbols']:
-        stim = lookup[symbol]
-        duration = stim.getDuration()
-        code = bite['code']
+        audio, visual = lookup[symbol]
         port.setData(code)
-        stim.play()
+        visual.draw()
+        win.flip()
+        audio.play()
         core.wait(duration)
+        win.flip()
+        core.wait(isi)
         port.setData(0)
-
+    core.wait(1.5)
 def playTrainingBlock(block):
     for bite in block['bites']:
         playBite(bite)
-    
+    win.flip()
 
 def playTestBlock(testBlock):
     global data
     random.shuffle(testBlock['bites'])
-    for bite in testBlock['bites']:
-        fixation.draw()
+    for bite in testBlock['bites'][-1:]:
         win.flip()
         playBite(bite)
         test.draw()
         win.flip()
         answer = event.waitKeys(keyList=['y','n'])[0]
-        data.append(answer)
+        data.append([bite['code'], answer])
 
 def playBlocks(blocks):
-    global mode
-    for block in blocks:
-      fixation.draw()
-      win.flip()
+    for block in blocks[-1:]:
       if block['id'] != 'test':
           playTrainingBlock(block)
           if block['id'] == 3:
@@ -178,6 +214,9 @@ def playBlocks(blocks):
           win.flip()
           event.waitKeys(keyList=['space'])
           playTestBlock(block)
+          output = open('data/'+info[0]+'_'+info[1]+'_'+info[2]+'.txt','w')
+          output.write(str(data))
+          output.close()
           goodbye.draw()
           win.flip()
           event.waitKeys(keyList=['space'])
